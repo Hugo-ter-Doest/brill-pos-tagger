8c584ad18f5c36f8180649b1f66532ec62267be8
Hugo W.L. ter Doest <hwl.ter.doest@gmail.com>
2015-02-03 20:12:07 +0100

    Added first test: against pos module

--- spec/Brill_POS_Tagger_spec.js
+++ spec/Brill_POS_Tagger_spec.js
@@ -1,5 +1,5 @@
 /*
-    Brill's POS Tagger main file
+    Unit test for Brill's POS Tagger: test against the pos module
     Copyright (C) 2014 Hugo W.L. ter Doest
 
     This program is free software: you can redistribute it and/or modify
@@ -15,34 +15,33 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-var Tagger = require("./lib/brill_pos_tagger");
 
-var base_folder = "/home/hugo/workspace/brill-pos-tagger";
+var pos = require('pos');
+var Brill_POS_Tagger = require('../index');
+
+var base_folder = "/home/hugo/Workspace/brill-pos-tagger";
 //var base_folder = "/Eclipse Workspace/brill-pos-tagger";
 var en_rules_file = base_folder + "/data/English/tr_from_posjs.txt";
 var en_lexicon_file = base_folder + "/data/English/lexicon_from_posjs.json";
-var en_ex_sentence = ["I", "see", "the", "man", "with", "the", "telescope"];
+var en_ex_sentences = [["I", "see", "the", "man", "with", "the", "telescope"]];
 
 var du_rules_file = base_folder + "/data/Dutch/brill_CONTEXTRULES.jg";
 var du_lexicon_file = base_folder + "/data/Dutch/brill_LEXICON.jg";
 var du_ex_sentence = ["ik", "zie", "de", "man", "met", "de", "verrekijker"];
 
-var tagger = new Tagger(en_lexicon_file, en_rules_file, function(error) {
-  if (error) {
-    console.log(error);
-  }
-  else {
-    console.log(JSON.stringify(tagger.tag(en_ex_sentence)));
-  }
+describe('Brill\'s POS Tagger', function() {
+  var bril_pos_tagger;
+  it('should initialise correctly', function(done) {
+    bril_pos_tagger = new Brill_POS_Tagger(en_lexicon_file, en_rules_file, function(error) {
+      done();
+    });
+  });
+  
+  en_ex_sentences.forEach(function(en_ex_sentence) {
+    it('should initialise tag sentences just like the old pos module', function() {
+      var taggedWords_expected = new pos.Tagger().tag(en_ex_sentence);
+      var taggedWords = bril_pos_tagger.tag(en_ex_sentence);
+      expect(taggedWords).toEqual(taggedWords_expected);
+    });
+  });
 });
-
-/*
-tagger = new Tagger(du_lexicon_file, du_rules_file, function(error) {
-  if (error) {
-    console.log(error);
-  }
-  else {
-    console.log(JSON.stringify(tagger.tag(du_ex_sentence)));
-  }
-});
-*/
