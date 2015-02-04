126c2390b3dfcb8bf38a41b4046a33ae2eb444f4
Hugo W.L. ter Doest <hwl.ter.doest@gmail.com>
2015-02-04 22:17:41 +0100

    After some bug fixing to old pos module, the new tagger works exactly like the old

--- spec/Brill_POS_Tagger_spec.js
+++ spec/Brill_POS_Tagger_spec.js
@@ -16,6 +16,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+var fs = require('fs');
+var natural = require('natural');
 var pos = require('pos');
 var Brill_POS_Tagger = require('../index');
 
@@ -30,24 +32,46 @@ var en_ex_sentences = [["I", "see", "the", "man", "with", "the", "telescope"],
                        ["He", "will", "not", "race", "the", "car"],
                        ["When", "will", "the", "race", "end"]
                       ];
+                      
+var en_ex_nyt_article = base_folder + "/data/English/NYT-20150205-picassos-granddaughter-plans-to-sell-art-worrying-the-market.txt";              
 
 var du_rules_file = base_folder + "/data/Dutch/brill_CONTEXTRULES.jg";
 var du_lexicon_file = base_folder + "/data/Dutch/brill_LEXICON.jg";
 var du_ex_sentence = ["ik", "zie", "de", "man", "met", "de", "verrekijker"];
 
 describe('Brill\'s POS Tagger', function() {
-  var bril_pos_tagger;
+  var old_brill_tagger = new pos.Tagger();
+  var brill_pos_tagger;
   it('should initialise correctly', function(done) {
-    bril_pos_tagger = new Brill_POS_Tagger(en_lexicon_file, en_rules_file, function(error) {
+    brill_pos_tagger = new Brill_POS_Tagger(en_lexicon_file, en_rules_file, function(error) {
       done();
     });
   });
-  
+
   en_ex_sentences.forEach(function(en_ex_sentence) {
-    it('should initialise tag sentences just like the old pos module', function() {
-      var taggedWords_expected = new pos.Tagger().tag(en_ex_sentence);
-      var taggedWords = bril_pos_tagger.tag(en_ex_sentence);
+    it('should process tag sentences just like the old pos module', function() {
+      var taggedWords_expected = old_brill_tagger.tag(en_ex_sentence);
+      var taggedWords = brill_pos_tagger.tag(en_ex_sentence);
       expect(taggedWords).toEqual(taggedWords_expected);
+    });
+  });
+
+  var sentences;
+  it('should correctly read a NYT article about Picasso', function(done) {
+    fs.readFile(en_ex_nyt_article, 'utf8', function (error, text) {
+      sentences = text.split('\n');
+      done();
+    });
+  });
+  var tokenizer = new natural.WordTokenizer();
+
+  it('should process tag sentences just like the old pos module', function() {
+    sentences.forEach(function(sentence){
+    var tokenized_sentence = tokenizer.tokenize(sentence);
+      var taggedWords_expected = old_brill_tagger.tag(tokenized_sentence);
+      var taggedWords = brill_pos_tagger.tag(tokenized_sentence);
+      expect(taggedWords).toEqual(taggedWords_expected);
+      console.log(taggedWords)
     });
   });
 });
