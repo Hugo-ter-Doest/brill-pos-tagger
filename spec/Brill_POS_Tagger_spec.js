84120fad139684a595b53000a26f92eebf6a7710
Hugo W.L. ter Doest <hwl.ter.doest@gmail.com>
2015-02-05 23:44:55 +0100

    Spec file is not ok!

--- spec/Brill_POS_Tagger_spec.js
+++ spec/Brill_POS_Tagger_spec.js
@@ -18,42 +18,26 @@
 
 var fs = require('fs');
 var natural = require('natural');
-var pos = require('pos');
 var Brill_POS_Tagger = require('../index');
 
 var base_folder = "/home/hugo/Workspace/brill-pos-tagger";
 //var base_folder = "/Eclipse Workspace/brill-pos-tagger";
 var en_rules_file = base_folder + "/data/English/tr_from_posjs.txt";
 var en_lexicon_file = base_folder + "/data/English/lexicon_from_posjs.json";
-var en_ex_sentences = [["I", "see", "the", "man", "with", "the", "telescope"],
-                       ["The", "run", "lasted", "thirty", "minutes"],
-                       ["We", "run", "three", "miles", "every", "day"],
-                       ["He", "will", "race", "the", "car"],
-                       ["He", "will", "not", "race", "the", "car"],
-                       ["When", "will", "the", "race", "end"]
-                      ];
                       
 var en_ex_nyt_article = base_folder + "/spec/NYT-20150205-picassos-granddaughter-plans-to-sell-art-worrying-the-market.txt";
 var en_ex_nyt_article_expected_tag_results = base_folder + '/spec/NYT-20150205-picassos-granddaughter-plans_expected_tag_results.txt';
 
 var du_rules_file = base_folder + "/data/Dutch/brill_CONTEXTRULES.jg";
 var du_lexicon_file = base_folder + "/data/Dutch/brill_LEXICON.jg";
-var du_ex_sentence = ["ik", "zie", "de", "man", "met", "de", "verrekijker"];
+
+var du_ex_volkskrant_article = base_folder + '/spec/Volkskrant-20150205-Knot-geldpers-aanzetten-is-paardenmiddel-voor-half-procent-inflatie.txt';
 
 describe('Brill\'s POS Tagger', function() {
-  var old_brill_tagger = new pos.Tagger();
   var brill_pos_tagger;
-  it('should initialise correctly', function(done) {
+  it('should initialise correctly with tagging rules for English', function(done) {
     brill_pos_tagger = new Brill_POS_Tagger(en_lexicon_file, en_rules_file, function(error) {
       done();
-    });
-  });
-
-  en_ex_sentences.forEach(function(en_ex_sentence) {
-    it('should process tag sentences just like the old pos module', function() {
-      var taggedWords_expected = old_brill_tagger.tag(en_ex_sentence);
-      var taggedWords = brill_pos_tagger.tag(en_ex_sentence);
-      expect(taggedWords).toEqual(taggedWords_expected);
     });
   });
 
@@ -65,6 +49,7 @@ describe('Brill\'s POS Tagger', function() {
     });
   });
   
+  var posjs_results;
   it('should correctly read tag results of pos-js for the NYT article', function(done) {
     fs.readFile(en_ex_nyt_article_expected_tag_results, 'utf8', function (error, text) {
       posjs_results = JSON.parse(text);
@@ -74,11 +59,38 @@ describe('Brill\'s POS Tagger', function() {
   
   var tokenizer = new natural.WordTokenizer();
 
-  it('should process tag the article just like the old pos module', function() {
+  it('should process the article just like the old pos module', function() {
     sentences.forEach(function(sentence, index) {
       var tokenized_sentence = tokenizer.tokenize(sentence);
       var taggedWords = brill_pos_tagger.tag(tokenized_sentence);
       expect(taggedWords).toEqual(posjs_results[index]);
     });
   });
+  
+  var brill_pos_tagger1;
+  it('should initialise correctly with tagging rules for Dutch', function(done) {
+    brill_pos_tagger1 = new Brill_POS_Tagger(du_lexicon_file, du_rules_file, function(error) {
+      if (error) {
+        console.log(error);
+      }
+      done();
+    });
+  });
+  
+  it('should correctly read a Volkskrant article about the ECB', function(done) {
+    fs.readFile(du_ex_volkskrant_article, 'utf8', function (error, text) {
+      sentences = text.split('\n');
+      done();
+    });
+  });
+  
+  it('should process the article', function() {
+    sentences.forEach(function(sentence, index) {
+      var tokenized_sentence = tokenizer.tokenize(sentence);
+      var taggedWords = brill_pos_tagger1.tag(tokenized_sentence);
+      //expect(taggedWords).toEqual(posjs_results[index]);
+      //console.log(taggedWords);
+    });
+  });
+  
 });
